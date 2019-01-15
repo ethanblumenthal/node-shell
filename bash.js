9c70c97a4ad07e4935eb091f525c6fe472660f0f
Sam Bengtson <sambeng1@gmail.com>
2019-01-15 11:35:28 -0600

    second commit

--- bash.js
+++ bash.js
@@ -1,10 +1,18 @@
 const pwd = require('./pwd');
+const ls = require('./ls')
+const cat = require('./cat')
 
 process.stdout.write('promt > ');
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
+  const cmd = data.toString().trim().split();
+  if (cmd[0] === 'pwd') {
     pwd();
+  } 
+  else if (cmd[0] === 'ls') {
+    ls();
+  } 
+  else if (cmd[0] === 'cat') {
+    cat(cmd[1]);
   } else {
     process.stdout.write('You typed: ' + cmd);
   }
---
+++ cat.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+
+module.exports = function(file) {
+  
+  fs.readFile(filePath)
+}
